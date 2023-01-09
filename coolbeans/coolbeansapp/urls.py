fc4b6f7f94868476ee08f31928e528df0a322123
P. Christopher Schreiber <pchristopherschreiber@gmail.com>
2023-01-09 03:33:48 -0500

    found a stray character 'g'

--- coolbeans/coolbeansapp/urls.py
+++ coolbeans/coolbeansapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from coolbeansapp.views import HomeView, OrderView, ConfirmationView, ReceiptView, ProductView
 
-urlpatterns = g[
+urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('place_order' , OrderView.as_view(), name='order'),
     path('confirmation', ConfirmationView.as_view(), name='confirmation'),
