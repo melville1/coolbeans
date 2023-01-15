b2ef7a62f645050dae2fc56d506a2dbfd12ffe01
Melvin <morel.melvin@gmail.com>
2023-01-15 13:02:41 -0500

    comments

--- coolbeans/coolbeansapp/views.py
+++ coolbeans/coolbeansapp/views.py
@@ -24,10 +24,12 @@ class OrderView(View):
     
 
     def get (self,request):
-                                    # The orders will be associated with table Addressee
+                                    # The orders will be associated with the Order table
                                     # second arguments specifies which table it will use to make forms
+                                    # we are then able to make one form from these 2 tables.
         OrderItemFormset = inlineformset_factory(Order,OrderItem, fields=['product','quantity'])
-    
+        #inlineformfactory allow you to create multiple forms at once. makes it more efficient.
+        # # the form will only contain attributes that are contain within orderitem table 
         formset = OrderItemFormset()
         form = OrderForm()
        
@@ -47,12 +49,23 @@ class OrderView(View):
     def post(self,request):
         OrderFormset = inlineformset_factory(Order,OrderItem, fields=['product','quantity'])
         Customer = request.POST["addressee"]
-        addressee = Addressee.objects.get(id=Customer) # retrieveing a specific addressee
-        order = Order.objects.create(addressee=addressee)
+        addressee = Addressee.objects.get(id=Customer) # we are getting the addresse - a set up for the following line
+        order = Order.objects.create(addressee=addressee) #order is created connecting it to the addressee
         formset = OrderFormset(request.POST, instance=order)
+         # we are calling the OrderItemFormset and filling it with the post data and then associating with the 
+        # order from line 54 the "instance" in this case means associating with the order
+        # what is happening here is that line 50 calls the form because the post needs it, then 
+        # we get the adresse id this is the set up for the follwing line (52)because we will then 
+        # create an order and associate it with the addressee and his/her info
+        #line 54 then passes the data that was created from the addresse which was created in the post and associates 
+        # to the order from line 53
         if formset.is_valid():
             formset.save()
         return redirect('confirmation', order.id )
+        # this redirects us to confirmation 
+        # the second argument "create.id" grabs the id from the order it does this because an object was created
+        # from line 53, therefore we not only access to the id but any other attribute that object may contain
+        # keep in mind this order.id now contains data
 
 
 class EditView(View):
@@ -61,6 +74,8 @@ class EditView(View):
         order = Order.objects.get(id=id)
         OrderFormset = inlineformset_factory(Order,OrderItem, fields=['product','quantity'],)
         formset = OrderFormset(instance=order)
+    # the order id is from the previous view here we are accessing and displaying that order 
+    # which is why you see it prefilled.
 
         html_data ={ 
             'formset':formset
@@ -81,7 +96,8 @@ class EditView(View):
         if formset.is_valid():
             formset.save()
         return redirect('confirmation', order.id )
-        
+        #line 95 -whatever new data was put in the post whether it remains the same or not gets resent to the form
+        # associated with that orderid 
 
 class ConfirmationView(View):
     def get (self,request,id): # the id is like a book but only contains the title
